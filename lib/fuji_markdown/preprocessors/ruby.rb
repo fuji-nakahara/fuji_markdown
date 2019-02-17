872b0801ae062a7a8bba6434caf04f2830218457
Fuji Nakahara <fujinakahara2032@gmail.com>
2019-02-17 15:41:07 +0900

    Refactor Preprocessors::Ruby

--- lib/fuji_markdown/preprocessors/ruby.rb
+++ lib/fuji_markdown/preprocessors/ruby.rb
@@ -4,16 +4,16 @@ module FujiMarkdown
       RUBY_PATTERN = %r((?<!\\){(?<kanji>.*?)\|(?<kana>.*?)}).freeze
 
       def call(text)
-        text.gsub!(RUBY_PATTERN) do |match|
-          kanji, *kanas = match.slice(1...-1).split('|')
+        text.gsub!(RUBY_PATTERN) do |str|
+          kanji, *kanas = str[/\A{(.*)}\z/, 1].split('|')
           return "<ruby>#{kanji}<rt></rt></ruby>" if kanas.empty?
 
           pairs = []
           kanas.each_with_index do |kana, i|
             if i == kanas.size - 1
-              pairs << [kanji.slice(i..-1), kana]
+              pairs << [kanji[i..-1], kana]
             else
-              pairs << [kanji.slice(i), kana]
+              pairs << [kanji[i], kana]
             end
           end
 
--- spec/fuji_markdown/preprocessors/ruby_spec.rb
+++ spec/fuji_markdown/preprocessors/ruby_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe FujiMarkdown::Preprocessors::Ruby do
   context 'with "{漢字|かんじ}"' do
     let(:text) { '{漢字|かんじ}' }
 
-    it 'converts it into "<ruby>漢字<rt>かんじ</rt></ruby>"' do
+    it 'should convert the text into "<ruby>漢字<rt>かんじ</rt></ruby>"' do
       subject
 
       expect(text).to eq '<ruby>漢字<rt>かんじ</rt></ruby>'
@@ -14,7 +14,7 @@ RSpec.describe FujiMarkdown::Preprocessors::Ruby do
   context 'with "{漢字|かん|じ}"' do
     let(:text) { '{漢字|かん|じ}' }
 
-    it 'converts it into "<ruby>漢<rt>かん</rt>字<rt>じ</rt></ruby>"' do
+    it 'should convert the text into "<ruby>漢<rt>かん</rt>字<rt>じ</rt></ruby>"' do
       subject
 
       expect(text).to eq '<ruby>漢<rt>かん</rt>字<rt>じ</rt></ruby>'
@@ -24,7 +24,7 @@ RSpec.describe FujiMarkdown::Preprocessors::Ruby do
   context 'with "{振仮名余|ふり|が|な|あま|り}"' do
     let(:text) { '{振仮名余|ふり|が|な|あま|り}' }
 
-    it 'converts it into "<ruby>振<rt>ふり</rt>仮<rt>が</rt>名<rt>な</rt>余<rt>あま</rt><rt>り</rt></ruby>"' do
+    it 'should convert the text into "<ruby>振<rt>ふり</rt>仮<rt>が</rt>名<rt>な</rt>余<rt>あま</rt><rt>り</rt></ruby>"' do
       subject
 
       expect(text).to eq '<ruby>振<rt>ふり</rt>仮<rt>が</rt>名<rt>な</rt>余<rt>あま</rt><rt>り</rt></ruby>'
@@ -34,7 +34,7 @@ RSpec.describe FujiMarkdown::Preprocessors::Ruby do
   context 'with "{漢字余|かん|じあまり}"' do
     let(:text) { '{漢字余|かん|じあまり}' }
 
-    it 'converts it into "<ruby>漢<rt>かん</rt>字余<rt>じあまり</rt></ruby>"' do
+    it 'should convert the text into "<ruby>漢<rt>かん</rt>字余<rt>じあまり</rt></ruby>"' do
       subject
 
       expect(text).to eq '<ruby>漢<rt>かん</rt>字余<rt>じあまり</rt></ruby>'
