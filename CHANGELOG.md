72705ae5bc1ed0e2545a64bc738e11f1baeb8a9a
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-10-25 00:46:59 +0900

    Stop holding presets as constants

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Stop holding presets as constants.
+
 ## [0.4.0] - 2020-10-12
 
 ### Added
--- lib/fuji_markdown.rb
+++ lib/fuji_markdown.rb
@@ -10,36 +10,41 @@ require_relative 'fuji_markdown/renderers/kakuyomu_renderer'
 require_relative 'fuji_markdown/renderers/narou_renderer'
 
 module FujiMarkdown
-  PRESETS = {
-    HTML: {
-      preprocessors: [Preprocessors::Ruby.new],
-      renderer: CommonMarker::HtmlRenderer.new(options: %i[HARDBREAKS UNSAFE])
-    },
-    KAKUYOMU: {
-      preprocessors: [Preprocessors::Ruby.new, proc { |text| text.gsub(/《/, '|《') }],
-      postprocessors: [Postprocessors::Ruby.new],
-      renderer: Renderers::KakuyomuRenderer.new
-    },
-    NAROU: {
-      preprocessors: [Preprocessors::Ruby.new, Preprocessors::EscapeNarou.new],
-      postprocessors: [Postprocessors::Ruby.new],
-      renderer: Renderers::NarouRenderer.new
-    }
-  }.freeze
-
   class << self
     def parse(text, preset = :HTML)
-      options = PRESETS.fetch(preset)
+      options = build_options_from_preset(preset)
       Processor.new(**options).parse(text)
-    rescue KeyError => e
-      raise InvalidPresetError, "Invalid preset: #{e.key}"
     end
 
     def render(text, preset = :HTML)
-      options = PRESETS.fetch(preset)
+      options = build_options_from_preset(preset)
       Processor.new(**options).render(text)
-    rescue KeyError => e
-      raise InvalidPresetError, "Invalid preset: #{e.key}"
+    end
+
+    private
+
+    def build_options_from_preset(preset) # rubocop:disable Metrics/MethodLength
+      case preset
+      when :HTML
+        {
+          preprocessors: [Preprocessors::Ruby.new],
+          renderer: CommonMarker::HtmlRenderer.new(options: %i[HARDBREAKS UNSAFE])
+        }
+      when :KAKUYOMU
+        {
+          preprocessors: [Preprocessors::Ruby.new, proc { |text| text.gsub(/《/, '|《') }],
+          postprocessors: [Postprocessors::Ruby.new],
+          renderer: Renderers::KakuyomuRenderer.new
+        }
+      when :NAROU
+        {
+          preprocessors: [Preprocessors::Ruby.new, Preprocessors::EscapeNarou.new],
+          postprocessors: [Postprocessors::Ruby.new],
+          renderer: Renderers::NarouRenderer.new
+        }
+      else
+        raise InvalidPresetError, "Invalid preset: #{preset}"
+      end
     end
   end
 end
